0ee01adde26933fec10b9611863041299c9bde24
Tanguy VANSNICK <191451@umons.ac.be>
2024-07-13 01:40:00 +0200

    python simple llm interaction

--- galadriel/contracts/contracts/scripts/settings.py
+++ galadriel/contracts/contracts/scripts/settings.py
@@ -5,9 +5,10 @@ load_dotenv()
 
 CHAIN_ID = int(os.getenv("CHAIN_ID", "696969"))
 RPC_URL = os.getenv("RPC_URL", "https://devnet.galadriel.com")
-PRIVATE_KEY = os.getenv("PRIVATE_KEY")
+PRIVATE_KEY = os.getenv("PRIVATE_KEY_GALADRIEL")
 STORAGE_KEY = os.getenv("PINATA_API_KEY")
 ORACLE_ADDRESS = os.getenv("ORACLE_ADDRESS")
+QUICKSTART_CONTRACT_ADDRESS="0x6EAC4c2c03698B286F1bb75bD74591eCdba270D4"
 ORACLE_ABI_PATH = os.getenv(
     "ORACLE_ABI_PATH",
     "../oracles/abi/ChatOracle.json",
