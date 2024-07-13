448f52bba789c1f4f82e0044eaefe4982c78b243
HemangVora <hemangvora2@gmail.com>
2024-07-13 02:52:44 +0200

    added custom pointer

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -24,7 +24,7 @@ export function NavbarComp() {
               >
                 About
               </a> */}
-              <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-white bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+              <button className="pointer-events-none inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-white bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-slate-50">
                 Launch App
               </button>
             </li>
