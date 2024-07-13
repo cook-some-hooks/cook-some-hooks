5b0ec0f272397379c346695891f1299768afeddf
cleanerzkp <0xcleaner@proton.me>
2024-07-13 16:10:04 +0200

    more chains

--- frontend/config/index.tsx
+++ frontend/config/index.tsx
@@ -1,7 +1,7 @@
 import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
 
 import { cookieStorage, createStorage } from "wagmi";
-import { mainnet, sepolia } from "wagmi/chains";
+import { mainnet, sepolia, baseSepolia, optimismSepolia } from "wagmi/chains";
 
 // Get projectId from https://cloud.walletconnect.com
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
@@ -9,14 +9,14 @@ export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 if (!projectId) throw new Error("Project ID is not defined");
 
 const metadata = {
-  name: "Web3Modal",
-  description: "Web3Modal Example",
+  name: "Cook a Hook",
+  description: "Let them cook",
   url: "https://web3modal.com", // origin must match your domain & subdomain
   icons: ["https://avatars.githubusercontent.com/u/37784886"],
 };
 
 // Create wagmiConfig
-const chains = [mainnet, sepolia, ] as const;
+const chains = [mainnet, sepolia, baseSepolia, optimismSepolia ] as const;
 export const config = defaultWagmiConfig({
   chains,
   projectId,
