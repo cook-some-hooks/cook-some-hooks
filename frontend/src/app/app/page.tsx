fc6282dc5833b6d5c20e49ee3c8509ac7ec3cbe1
HemangVora <hemangvora2@gmail.com>
2024-07-13 13:31:50 +0200

    design updated aap

--- frontend/src/app/app/page.tsx
+++ frontend/src/app/app/page.tsx
@@ -17,6 +17,7 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Select } from "@/components/ui/select";
 import { Option } from "@/components/ui/option";
+import { TextArea } from "@/components/ui/textarea";
 export default function Home() {
   const { address, isConnected } = useAccount();
   const { disconnect } = useDisconnect();
@@ -28,72 +29,49 @@ export default function Home() {
 
   return (
     <main className="">
-      <FollowerPointerCard>
-        <NavbarApp />
-        <HeroHighlight>
-          <div className="flex flex-row items-center justify-center">
-            <div className="max-w-md w-full  rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
-              <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-                'Cook your Hoook
-              </h2>
-              <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300"></p>
+      <NavbarApp />
+      <HeroHighlight>
+        <div className="flex flex-row items-center justify-center">
+          <div className=" w-[400px]  rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+            <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+              'Cook your Hoook
+            </h2>
 
-              <form className="my-8" onSubmit={handleSubmit}>
-                <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-                  <LabelInputContainer>
-                    {/* <Label htmlFor="firstname">First name</Label> */}
-                    <Input id="firstname" placeholder="Tyler" type="text" />
-                  </LabelInputContainer>
-                  <LabelInputContainer>
-                    {/* <Label htmlFor="lastname">Last name</Label> */}
-                    {/* <Input id="lastname" placeholder="Durden" type="text" /> */}
-                    <Select>
-                      <option value="1">1</option>
-                    </Select>
-                  </LabelInputContainer>
-                </div>
-                <LabelInputContainer className="mb-4">
-                  <Label htmlFor="email">Email Address</Label>
-                  <Input
-                    id="email"
-                    placeholder="projectmayhem@fc.com"
-                    type="email"
-                  />
+            <form className="my-8" onSubmit={handleSubmit}>
+              <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+                <LabelInputContainer>
+                  <Select>
+                    <option value="1">1</option>
+                  </Select>
                 </LabelInputContainer>
-                <LabelInputContainer className="mb-4">
-                  <Label htmlFor="password">Password</Label>
-                  <Input id="password" placeholder="••••••••" type="password" />
+                <LabelInputContainer>
+                  {/* <Label htmlFor="lastname">Last name</Label> */}
+
+                  <Select>
+                    <option value="1">1</option>
+                  </Select>
                 </LabelInputContainer>
-                <LabelInputContainer className="mb-8">
-                  <Label htmlFor="twitterpassword">Your twitter password</Label>
-                  <Input
-                    id="twitterpassword"
-                    placeholder="••••••••"
-                    type="twitterpassword"
-                  />
-                </LabelInputContainer>
+              </div>
+              <LabelInputContainer className="mb-4">
+                <Label htmlFor="prompt">Hook prompt</Label>
+                <TextArea
+                  id="prompt"
+                  placeholder="Enter your prompt here for cooking your hook "
+                />
+              </LabelInputContainer>
 
-                <button
-                  className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-                  type="submit"
-                >
-                  Sign up &rarr;
-                  <BottomGradient />
-                </button>
+              <button
+                className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+                type="submit"
+              >
+                Generate with OpenAI &rarr;
+                <BottomGradient />
+              </button>
 
-                <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+              <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
 
-                <div className="flex flex-col space-y-4">
-                  <button
-                    className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-                    type="submit"
-                  >
-                    <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-                    <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-                      GitHub
-                    </span>
-                    <BottomGradient />
-                  </button>
+              {/* <div className="flex flex-col space-y-4">
+                  
                   <button
                     className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
                     type="submit"
@@ -114,12 +92,11 @@ export default function Home() {
                     </span>
                     <BottomGradient />
                   </button>
-                </div>
-              </form>
-            </div>
+                </div> */}
+            </form>
           </div>
-        </HeroHighlight>
-      </FollowerPointerCard>
+        </div>
+      </HeroHighlight>
     </main>
   );
 }
--- frontend/src/components/NavbarApp.tsx
+++ frontend/src/components/NavbarApp.tsx
@@ -39,7 +39,7 @@ export function NavbarApp() {
               </a> */}
               {!isConnected && (
                 <button
-                  className="cursor-none inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-white bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+                  className="cursor-pointer inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-white bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-slate-50"
                   onClick={() => modal.open()}
                 >
                   Connect Wallet
