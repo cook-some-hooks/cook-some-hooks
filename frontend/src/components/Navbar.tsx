a5c2a463fed841183f5505e656247de6e4726f5c
cleanerzkp <0xcleaner@proton.me>
2024-07-13 23:00:34 +0200

    chain selector

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,9 +4,23 @@ import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "../../lib/utils";
 import ConnectButton from "./connect";
 import { useRouter } from "next/navigation";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
+import { useAccount, useDisconnect } from "wagmi";
+import ChainToggle from "./ChainToggle"; // Ensure the path is correct
 
 export function NavbarComp() {
+  const modal = useWeb3Modal();
+  const { address, isConnected } = useAccount();
   const navigate = useRouter();
+  const { disconnect } = useDisconnect();
+
+  function shortenAddress(address: string) {
+    if (!address) return "";
+    const start = address.slice(0, 6);
+    const end = address.slice(-4);
+    return `${start}...${end}`;
+  }
+
   return (
     <nav className="bg-white border-gray-200 dark:bg-black bg-opacity-15 bg-transparent backdrop-blur-sm">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -22,27 +36,33 @@ export function NavbarComp() {
           </span>
         </a>
 
-        <div className=" w-full md:block md:w-auto" id="navbar-default">
+        <div className="w-full md:block md:w-auto" id="navbar-default">
+        <ChainToggle />
+
           <ul className="font-Jetmono font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-black dark:border-gray-700">
             <li>
-              {/* <a
-                href="#"
-                className="block py-2 px-3 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                About
-              </a> */}
-              <button
-                className=" inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-white/[0.2] bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-slate-50"
-                onClick={() => {
-                  navigate.push("/app");
-                }}
-              >
-                Launch App
-              </button>
-
+              
+              {!isConnected && (
+                <button
+                  className="cursor-pointer inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-white/[0.2] bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+                  onClick={() => modal.open()}
+                >
+                  Connect Wallet
+                </button>
+              )}
+              {isConnected && (
+                <div className="flex items-center space-x-4">
+                  <span>{shortenAddress(address || "")}</span>
+                  <span
+                    className="text-red-500 cursor-pointer"
+                    onClick={() => disconnect()}
+                  >
+                    X
+                  </span>
+                </div>
+              )}
             </li>
           </ul>
-          
         </div>
       </div>
     </nav>
@@ -68,7 +88,7 @@ function Navbar({ className }: { className?: string }) {
           </div>
         </MenuItem>
         <MenuItem setActive={setActive} active={active} item="Products">
-          <div className="  text-sm grid grid-cols-2 gap-10 p-4">
+          <div className="text-sm grid grid-cols-2 gap-10 p-4">
             <ProductItem
               title="Algochurn"
               href="https://algochurn.com"
@@ -107,3 +127,5 @@ function Navbar({ className }: { className?: string }) {
     </div>
   );
 }
+
+export default NavbarComp;
