a4a6ac5fad7fbf3a11f1010788e0d716394b0abc
HemangVora <hemangvora2@gmail.com>
2024-07-13 02:52:44 +0200

    added custom pointer

--- frontend/src/components/ui/navbar-menu.tsx
+++ frontend/src/components/ui/navbar-menu.tsx
@@ -28,7 +28,7 @@ export const MenuItem = ({
     <div onMouseEnter={() => setActive(item)} className="relative ">
       <motion.p
         transition={{ duration: 0.3 }}
-        className="cursor-pointer text-black hover:opacity-[0.9] dark:text-white"
+        className=" text-black hover:opacity-[0.9] dark:text-white"
       >
         {item}
       </motion.p>
