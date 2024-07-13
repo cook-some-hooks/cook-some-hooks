724fd2875997438fb09bad0b011ad6e1b97a6b7e
HemangVora <hemangvora2@gmail.com>
2024-07-13 02:41:13 +0200

    added navbar

--- frontend/src/components/connect.tsx
+++ frontend/src/components/connect.tsx
@@ -1,3 +1,11 @@
+import { useWeb3Modal } from "@web3modal/wagmi/react";
+
 export default function ConnectButton() {
-  return <w3m-button />;
+  const modal = useWeb3Modal();
+
+  return (
+    <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+      <button onClick={() => modal.open()}>Connect Wallet</button>
+    </button>
+  );
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -26,10 +26,26 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        Jetmono: ["JetBrains Mono", "sans-serif"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      animation: {
+        shimmer: "shimmer 2s linear infinite",
+      },
+      keyframes: {
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
       },
     },
   },
