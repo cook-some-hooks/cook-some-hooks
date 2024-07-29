f9b2d76dd890a57de0a4f2899e2be84387d3a8e3
Tanguy VANSNICK <191451@umons.ac.be>
2024-07-29 18:11:24 +0200

    added requirements, change hard coded path

--- foundry_hook_playground/src/generated/generated_hook_0.sol
+++ foundry_hook_playground/src/generated/generated_hook_0.sol
@@ -1,21 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.15;
 
-import {BaseHook} from "v4-periphery/BaseHook.sol";
 import {Hooks} from "v4-core/src/libraries/Hooks.sol";
-import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {BaseHook} from "v4-periphery/BaseHook.sol";
 import {PoolKey} from "v4-core/src/types/PoolKey.sol";
-import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
-import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
 import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
 
-contract SimpleHook is BaseHook {
-    using PoolIdLibrary for PoolKey;
-
-    mapping(PoolId => uint256) public beforeSwapCount;
-    mapping(PoolId => uint256) public afterSwapCount;
-    mapping(PoolId => uint256) public beforeAddLiquidityCount;
-    mapping(PoolId => uint256) public beforeRemoveLiquidityCount;
+contract After6PMHook is BaseHook {
+    uint256 public constant startingTime = 60 * 60 * 18; // 18:00 (6pm)
 
     constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
 
@@ -23,12 +16,12 @@ contract SimpleHook is BaseHook {
         return Hooks.Permissions({
             beforeInitialize: false,
             afterInitialize: false,
-            beforeAddLiquidity: true,
+            beforeAddLiquidity: false,
+            beforeRemoveLiquidity: false,
             afterAddLiquidity: false,
-            beforeRemoveLiquidity: true,
             afterRemoveLiquidity: false,
             beforeSwap: true,
-            afterSwap: true,
+            afterSwap: false,
             beforeDonate: false,
             afterDonate: false,
             beforeSwapReturnDelta: false,
@@ -38,46 +31,20 @@ contract SimpleHook is BaseHook {
         });
     }
 
-    // Increment the beforeSwapCount each time before a swap occurs
-    function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, bytes calldata)
+    function beforeSwap(address, PoolKey calldata, IPoolManager.SwapParams calldata, bytes calldata)
         external
+        view
         override
         returns (bytes4, BeforeSwapDelta, uint24)
     {
-        beforeSwapCount[key.toId()]++;
+        uint256 time = block.timestamp % (60 * 60 * 24);
+
+        require(
+            time >= startingTime,
+            "After6PMHook.beforeSwap: market not started yet"
+        );
+
         return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
-    }
-
-    // Increment the afterSwapCount each time after a swap occurs
-    function afterSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, BalanceDelta, bytes calldata)
-        external
-        override
-        returns (bytes4, int128)
-    {
-        afterSwapCount[key.toId()]++;
-        return (BaseHook.afterSwap.selector, 0);
-    }
-
-    // Increment the beforeAddLiquidityCount each time before liquidity is added
-    function beforeAddLiquidity(
-        address,
-        PoolKey calldata key,
-        IPoolManager.ModifyLiquidityParams calldata,
-        bytes calldata
-    ) external override returns (bytes4) {
-        beforeAddLiquidityCount[key.toId()]++;
-        return BaseHook.beforeAddLiquidity.selector;
-    }
-
-    // Increment the beforeRemoveLiquidityCount each time before liquidity is removed
-    function beforeRemoveLiquidity(
-        address,
-        PoolKey calldata key,
-        IPoolManager.ModifyLiquidityParams calldata,
-        bytes calldata
-    ) external override returns (bytes4) {
-        beforeRemoveLiquidityCount[key.toId()]++;
-        return BaseHook.beforeRemoveLiquidity.selector;
     }
 
    function validateHookAddress(BaseHook _this) internal pure override {
