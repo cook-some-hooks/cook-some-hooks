06e64a41df3e7234c3cf3f4cc149e47ead25a304
Tanguy VANSNICK <191451@umons.ac.be>
2024-08-10 16:54:18 +0200

    rename path

--- foundry_hook_playground/src/generated/generated_hook_0.sol
+++ foundry_hook_playground/src/generated/generated_hook_0.sol
@@ -5,54 +5,54 @@ import {BaseHook} from "v4-periphery/BaseHook.sol";
 import {Hooks} from "v4-core/src/libraries/Hooks.sol";
 import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
 import {PoolKey} from "v4-core/src/types/PoolKey.sol";
-import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
-import {Currency} from "v4-core/src/types/Currency.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MintRewardHook is BaseHook {
-    using PoolIdLibrary for PoolKey;
+contract RewardMintHook is BaseHook, ERC20 {
+    uint256 public constant REWARD_AMOUNT = 1000 * 10 ** 18;
 
-    // Reward token
-    IERC20 public immutable rewardToken;
-    
-    // Reward amount per mint
-    uint256 public constant REWARD_AMOUNT = 10 * 10**18; // 10 tokens
+    constructor(
+        IPoolManager _poolManager,
+        string memory _name,
+        string memory _symbol
+    ) BaseHook(_poolManager) ERC20(_name, _symbol) {}
 
-    constructor(IPoolManager _poolManager, IERC20 _rewardToken) BaseHook(_poolManager) {
-        rewardToken = _rewardToken;
+    function getHookPermissions()
+        public
+        pure
+        override
+        returns (Hooks.Permissions memory)
+    {
+        return
+            Hooks.Permissions({
+                beforeInitialize: false,
+                afterInitialize: false,
+                beforeAddLiquidity: false,
+                beforeRemoveLiquidity: false,
+                afterAddLiquidity: false,
+                afterRemoveLiquidity: false,
+                beforeSwap: false,
+                afterSwap: false,
+                beforeSwap: false,
+                afterSwap: false,
+                beforeDonate: false,
+                afterDonate: false,
+                beforeSwapReturnDelta: false,
+                afterSwapReturnDelta: false,
+                afterAddLiquidityReturnDelta: false,
+                afterRemoveLiquidityReturnDelta: false
+            });
     }
 
-    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
-        return Hooks.Permissions({
-            beforeInitialize: false,
-            afterInitialize: false,
-            beforeAddLiquidity: true,
-            afterAddLiquidity: false,
-            beforeRemoveLiquidity: false,
-            afterRemoveLiquidity: false,
-            beforeSwap: false,
-            afterSwap: false,
-            beforeDonate: false,
-            afterDonate: false,
-            beforeSwapReturnDelta: false,
-            afterSwapReturnDelta: false,
-            afterAddLiquidityReturnDelta: false,
-            afterRemoveLiquidityReturnDelta: false
-        });
-    }
-
-    function beforeAddLiquidity(
-        address sender,
+    function afterInitialize(
+        address,
         PoolKey calldata,
-        IPoolManager.ModifyLiquidityParams calldata,
         bytes calldata
-    ) external override returns (bytes4) {
-        // Reward the minter
-        require(rewardToken.transfer(sender, REWARD_AMOUNT), "Reward transfer failed");
-        
-        return BaseHook.beforeAddLiquidity.selector;
+    ) external override onlyByManager returns (bytes4) {
+        // Mint reward tokens to the minter
+        _mint(msg.sender, REWARD_AMOUNT);
+        return BaseHook.afterInitialize.selector;
     }
 
-   function validateHookAddress(BaseHook _this) internal pure override {
-            }
-        }
+    function validateHookAddress(BaseHook _this) internal pure override {}
+}
